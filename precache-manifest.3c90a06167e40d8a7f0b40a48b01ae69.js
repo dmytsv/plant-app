b111bf996a0e8436aebac1a0382c1ca2020b509a
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-26 00:16:56 -0400

    Updates

---
+++ precache-manifest.3c90a06167e40d8a7f0b40a48b01ae69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b70c5ae501c38c02c6b1710e90bfa45",
+    "url": "/plant-app/index.html"
+  },
+  {
+    "revision": "57adc2739aa94c726f5b",
+    "url": "/plant-app/static/css/main.6ec210f6.chunk.css"
+  },
+  {
+    "revision": "6cfef3c79632364c4519",
+    "url": "/plant-app/static/js/2.ade8b935.chunk.js"
+  },
+  {
+    "revision": "57adc2739aa94c726f5b",
+    "url": "/plant-app/static/js/main.56be90a1.chunk.js"
+  },
+  {
+    "revision": "72fe75aba75b1abab2b1",
+    "url": "/plant-app/static/js/runtime~main.379239be.js"
+  }
+]);
